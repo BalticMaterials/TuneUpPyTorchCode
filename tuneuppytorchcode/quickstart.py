8d0901eaf6283330cb83543f1f7f10b5f9a8e2a8
Tjark <ziehm@balticmaterials.de>
2024-03-26 20:23:11 +0100

    update

--- tuneuppytorchcode/quickstart.py
+++ tuneuppytorchcode/quickstart.py
@@ -141,9 +141,16 @@ classes = [
     "Ankle boot",
 ]
 
+import time
+start_time = time.time()
+
 model.eval()
 x, y = test_data[0][0], test_data[0][1]
 with torch.no_grad():
     pred = model(x)
     predicted, actual = classes[pred[0].argmax(0)], classes[y]
     print(f'Predicted: "{predicted}", Actual: "{actual}"')
+
+end_time = time.time()
+execution_time = (start_time - end_time)* -1
+print("Execution time:",execution_time)
