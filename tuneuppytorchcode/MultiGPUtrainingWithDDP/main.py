4fcf2acf1d3bc11ee7dc8afea6885ba64655aa91
Tjark <ziehm@balticmaterials.de>
2024-03-26 18:42:52 +0100

    add Time messurment to MultiGPU

--- tuneuppytorchcode/MultiGPUtrainingWithDDP/main.py
+++ tuneuppytorchcode/MultiGPUtrainingWithDDP/main.py
@@ -94,11 +94,18 @@ def main(rank: int, world_size: int, save_every: int, total_epochs: int, batch_s
 
 if __name__ == "__main__":
     import argparse
+    import time
     parser = argparse.ArgumentParser(description='simple distributed training job')
     parser.add_argument('total_epochs', type=int, help='Total epochs to train the model')
     parser.add_argument('save_every', type=int, help='How often to save a snapshot')
     parser.add_argument('--batch_size', default=32, type=int, help='Input batch size on each device (default: 32)')
     args = parser.parse_args()
     
+    start_time = time.time()
+
     world_size = torch.cuda.device_count()
+    
     mp.spawn(main, args=(world_size, args.save_every, args.total_epochs, args.batch_size), nprocs=world_size)
+    end_time = time.time()
+    execution_time = (start_time - end_time)* -1
+    print("Execution time:",execution_time)
